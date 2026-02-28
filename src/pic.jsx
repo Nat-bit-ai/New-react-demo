a19853b96c7514fa955e5f2cb0c646ad3c35095a
Natnael Zerihun <nathyzer21@gmail.com>
2026-02-28 01:33:51 -0800

    click event for the picture

--- src/pic.jsx
+++ src/pic.jsx
@@ -1,8 +1,9 @@
 function ProfilePic (){
     const imgUrl = "./src/assets/react.svg";
+    const handleClick = (e) => e.target.style.display = "none";
     return(
     <>
-      <img src={imgUrl}></img>
+      <img src={imgUrl} onClick={(e) => handleClick(e)}></img>
     </>
     )
 }
