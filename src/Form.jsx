558c2fdbd0618faeacd5db6210c9ec2d971e07b4
Natnael Zerihun <nathyzer21@gmail.com>
2026-02-28 03:59:53 -0800

    updating data display from console to the screen

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,33 +1,71 @@
-import React , {useState} from "react"
+import React, { useState } from "react";
 
-function MyForm(){
-    const [name,setName] = useState("")
-    const [age, setAge] = useState("")
-    const [isEmployed, setIsEmployed] = useState(false)
+function MyForm() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [isEmployed, setIsEmployed] = useState(false);
+  const [collectData, setCollectedData] = useState(null);
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        const confirmName = prompt("please confirm your name");
-        if (confirmName !== name) {
-             console.log("name doesn't match")
-        return;
-        }
-        console.log("Submitted data")
-        console.log(`Name: ${name} `)
-        console.log(`Age: ${age}`)
-        console.log(`Employment-status: ${isEmployed}`)
+  const handleClick = (e) => {
+    e.preventDefault();
 
+    const confirmName = prompt("please confirm your name");
 
+    if (confirmName !== name) {
+      console.log("name doesn't match");
+      return;
     }
-    return(
-        <form onSubmit={handleClick}>
-            <input type="text" value={name} placeholder="Enter name" onChange={(e)=> setName(e.target.value)}/>
-            <input type="number" value={age} placeholder="Enter age" onChange= {(e)=> setAge(e.target.value)} />
-            <label>Employed:
-                <input type="checkbox" checked={isEmployed} onChange={(e)=> setIsEmployed(e.target.checked)} />
-            </label>
-            <button type="submit">Send</button>
-        </form>
-    )
+
+    console.log("Submitted data");
+
+    setCollectedData({
+      name,
+      age,
+      isEmployed
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Enter name"
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="number"
+          value={age}
+          placeholder="Enter age"
+          onChange={(e) => setAge(e.target.value)}
+        />
+
+        <label>
+          Employed:
+          <input
+            type="checkbox"
+            checked={isEmployed}
+            onChange={(e) => setIsEmployed(e.target.checked)}
+          />
+        </label>
+
+        <button type="submit">Send</button>
+      </form>
+
+      {collectData && (
+        <div>
+          <h2>DATA Submitted</h2>
+          <p>Name: {collectData.name}</p>
+          <p>Age: {collectData.age}</p>
+          <p>
+            Employment_status: {collectData.isEmployed ? "Yes" : "No"}
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
-export default MyForm
+
+export default MyForm;
