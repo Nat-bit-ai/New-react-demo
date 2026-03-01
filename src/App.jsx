672e5eb48f82c5d057813e9c4aa16b8756096d50
Natnael Zerihun <nathyzer21@gmail.com>
2026-03-01 05:04:14 -0800

    Counter with react

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import ColorPicker from "./color.jsx";
 import ProfilePic from "./pic.jsx";
 import Button from "./clickBtn.jsx";
 import MyForm from "./Form.jsx";
+import Counter from "./counter.jsx";
 
 function App() {
   return (
     <div>
-      <MyForm />
+      <Counter />
     </div>
   );
 }
