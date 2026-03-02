cc82ca9577663ec5f7177962516dd73d5738fe65
Natnael Zerihun <nathyzer21@gmail.com>
2026-03-03 01:46:47 +0300

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import MyForm from "./Form.jsx";
 import Counter from "./counter.jsx";
 import DigitalClock from "./digitalClock.jsx";
 import OTPGenerator  from "./otpGenerator.jsx";
+import {BrowserRouter , Routes , Route, Link} from 'react-router-dom';
+import Home from "./pages/home.jsx";
+import About from "./pages/about.jsx";
+
 
 function App() {
   return (
-    <div>
-      <OTPGenerator />
-    </div>
+    <BrowserRouter>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element= {<Home/>}/>
+      <Route path="/about" element={<About />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/about.jsx
@@ -0,0 +1,8 @@
+function About(){
+    return(
+        <>
+          <h2>We're Here Ethiopia</h2>
+        </>
+    )
+}
+export default About
